0b9c74dff7f3021d48bcffc335a16358dc200c13
MrCreeps <75145015+MrCreeps@users.noreply.github.com>
2023-02-19 15:27:58 -0600

    Create grade_calculator.py

---
+++ grade_calculator.py
@@ -0,0 +1,10 @@
+# Calculates grade based on the amount of problems and the amount of problems missed.
+
+try:
+  problems = int(input("How many problems are on the assignment? "))
+  missed = int(input("How many problems did you miss? "))
+except ValueError:
+  print("Not a number..."
+  exit()
+grade = 100 - ((100 / problems) * missed)
+print(f"You got a {round(grade, 1)}!")
