5c3876dafb6384641cc1cb809142a226a26e7e72
MrCreeps <75145015+MrCreeps@users.noreply.github.com>
2023-02-21 16:12:00 -0600

    Update naos.py

--- naos.py
+++ naos.py
@@ -10,8 +10,10 @@
 ##############################
 
 import os
+import json
+import time
 
-vers = "0.0.7"
+vers = "0.1.0"
 mainLoop = True
 
 # Temporary password storage initialization
@@ -22,18 +24,21 @@ passwords = {"user" : "123"}
 #####   User Policies   #####
 #############################
 
-quickLogin = True
+with open("userpolicies.json", "r") as file:
+    userPolicies = json.load(file)
+
+quickLogin = userPolicies["quickLogin"]
 if quickLogin:
     passwords[""] = ""
 
-funUsers = True
+funUsers = userPolicies["funUsers"]
 if funUsers:
     passwords["snakamoto"] = "btc"
     passwords["vbuterin"] = "eth"
     passwords["rulbricht"] = "dpr"
     passwords["esnowden"] = "wiki"
 
-adminEnabled = True
+adminEnabled = userPolicies["adminEnabled"]
 if adminEnabled:
     passwords["admin"] = "123"
 
@@ -55,7 +60,7 @@ def clear():
     
 def tempuser():
     tempUsername = input("Temporary username? ")
-    tempPassword = input(f"Username for {tempUsername}? ")
+    tempPassword = input(f"Password for {tempUsername}? ")
     if tempUsername not in passwords:
         passwords[tempUsername] = tempPassword
         print(f"New temporary user with password '{tempPassword}' and username '{tempUsername}' created!")
@@ -75,32 +80,68 @@ def listall():
         print (user)
         
 def userpolicies():
-    print(f"""quick_login = {quickLogin}
+    print(f""" 1 = True ; 0 = False
+quickLogin = {quickLogin}
 funUsers = {funUsers}
 adminEnabled = {adminEnabled}""")
 
-#########################
-#####   Main Loop   #####
-#########################
+def setpolicy():
+    policyName = input("Policy to change? ")
+    if userPolicies[policyName] == 0:
+        userPolicies[policyName] = 1
+    else:
+        userPolicies[policyName] = 0
+    with open("userpolicies.json", "w") as file:
+        json.dump(userPolicies, file)
 
-print(f"NAOS.X {vers}\n\n")
+def shutdown():
+    print("Shutting down in 3 seconds...")
+    time.sleep(3)
+    exit()
 
-try:
-    user = input("Username? ")
-    
-    inputPassword = input("Password? ")
-    if user not in passwords or inputPassword != passwords[user]:
-        raise wrongPasswordError()
+#####################################
+#####   Main Loop Definitions   #####
+#####################################
 
-    while mainLoop:
-        cmd = input(f"{user}>> ")
-        if cmd == "clear": clear()
-        elif cmd == "tempuser": tempuser()
-        elif cmd == "logout": user = logout()
-        elif cmd == "listall" and user == "admin": listall()
-        elif cmd == "userpolicies" and user == "admin": userpolicies()
-        else: print("Invalid command or permission level")
+def restart():
+    print("Restarting in 3 seconds...")
+    time.sleep(3)
+    os.system("cls")
+    main()
 
-except wrongPasswordError: print("Incorrect username or password")
-except userExistsError: print("Cannot add user as user already exists")
-except: exit()
+def main():
+    print(f"NAOS.X {vers}\n\n")
+    if quickLogin: print("Quick Login is enabled. Press enter twice to login.")
+
+    try:
+        user = input("Username? ")
+        
+        inputPassword = input("Password? ")
+        if user not in passwords or inputPassword != passwords[user]:
+            raise wrongPasswordError()
+
+        while mainLoop:
+            cmd = input(f"{user}>> ")
+            if cmd == "help": pass
+            elif cmd == "clear": clear()
+            elif cmd == "tempuser": tempuser()
+            elif cmd == "logout": user = logout()
+            elif cmd == "listall" and user == "admin": listall()
+            elif cmd == "userpolicies" and user == "admin": userpolicies()
+            elif cmd == "setpolicy" and user == "admin": setpolicy()
+            elif cmd == "restart": restart()
+            elif cmd == "shutdown": shutdown()
+            else: print("Invalid command or permission level")
+
+    except wrongPasswordError: print("Incorrect username or password") ; restart()
+    except userExistsError: print("Cannot add user as user already exists") ; restart()
+    except KeyError: print("Policy not available.") ; restart()
+    except:
+        restart()
+
+###########################
+#####   Entry Point   #####
+###########################
+
+if __name__ == "__main__":
+    main()
